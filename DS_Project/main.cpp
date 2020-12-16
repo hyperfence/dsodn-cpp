e4f733a5e9d981ff3c5031dec13a979995335cf4
Hunaid Sohail <hunaid2000@outlook.com>
2020-12-16 13:03:19 +0500

    Changed hello world.

--- DS_Project/main.cpp
+++ DS_Project/main.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-	std::cout << "Hello World!" << std::endl;
+	std::cout << "Hello Country!" << std::endl;
 	std::cout << "Correcting Workflow!" << std::endl;
 	std::cout << "Correcting Workflow!" << std::endl;
 	return 0;
