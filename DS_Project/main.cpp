b4d57a9cf677fcb1ee3b11ee86ab4834b756172a
Hunaid Sohail <hunaid2000@outlook.com>
2020-12-19 14:10:09 +0500

    Added machines and routingTable class in testing.cpp and modified ringDHT class in testing.cpp according to machines class

--- DS_Project/main.cpp
+++ DS_Project/main.cpp
@@ -1,32 +1,32 @@
 #include <iostream>
 #include "Machines.h"
 
-int main()
-{
-	int identifierSpace = 32;
-	Machines<int> m(identifierSpace);
-	m.insert(1);
-	m.insert(28);
-	m.insert(9);
-	m.insert(11);
-	m.insert(4);
-	m.insert(21);
-	m.insert(20);
-	m.insert(14);
-	m.insert(18);
-	m.display();
-	m.configureRoutingTable();
-	Machine_Node<int>* temp = m.searchData(12, 28);
-	if (temp != NULL)
-	{
-		cout << "Data is at Machine: " << temp->data << endl;
-	}
-	else
-	{
-		cout << "Couldn't find the data!" << endl;
-	}
-	return 0;
-}
+//int main()
+//{
+//	int identifierSpace = 32;
+//	Machines<int> m(identifierSpace);
+//	m.insert(1);
+//	m.insert(28);
+//	m.insert(9);
+//	m.insert(11);
+//	m.insert(4);
+//	m.insert(21);
+//	m.insert(20);
+//	m.insert(14);
+//	m.insert(18);
+//	m.display();
+//	m.configureRoutingTable();
+//	Machine_Node<int>* temp = m.searchData(12, 28);
+//	if (temp != NULL)
+//	{
+//		cout << "Data is at Machine: " << temp->data << endl;
+//	}
+//	else
+//	{
+//		cout << "Couldn't find the data!" << endl;
+//	}
+//	return 0;
+//}
 
 
 
